191bc0ab11d020759d90258605cff67b12dfd448
Janishnehyan03 <nehyanjanish@gmail.com>
2023-11-14 20:21:25 +0530

    updated

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,51 +1,71 @@
-"use client";
-import React, { useState } from "react";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-
 function Navbar() {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen);
-  };
-
   return (
-    <div className="bg-[#fff] bg-opacity-80">
-      <div
-        className="mx-auto flex items-center justify-between p-3 lg:px-8"
-        aria-label="Global"
-      >
-        <div className="flex">
-          <Link href="/" className="-m-1.5 hidden lg:flex p-1.5">
-            <img className="h-20" src="/logo dh.png" alt="" />
-          </Link>
-          <Link
-            href="/"
-            className="-m-1.5 lg:hidden flex items-center justify-between p-1.5"
-          >
-            <img className="h-20 mr-3" src="/logo.png" alt="" />
-            <div className="text-center">
-              <h1 className="font-bold text-[10px] text-center uppercase text-[#251e74]">
-                Office Of the Controller Of Examinations
-              </h1>
-              <h1 className="text-[#251e74]  font-bold text-xl">
-                مكتب المراقب الامتحانات
-              </h1>
-            </div>
-          </Link>
-        </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+    <div className="bg-[#fff] bg-opacity-80 p-4">
+      <div className="flex items-center justify-between">
+        <Link href="/" className="-m-1.5 hidden lg:flex p-1.5">
+          <img className="h-20" src="/logo dh.png" alt="" />
+        </Link>
+
+        <Link
+          href="/"
+          className="-m-1.5 lg:hidden flex items-center justify-between p-1.5"
+        >
+          <img className="h-20 mr-3" src="/logo.png" alt="" />
           <div className="text-center">
-            <h1 className="font-bold text-[15px] text-center uppercase text-[#251e74]">
+            <h1 className="font-bold text-[10px] text-center uppercase text-[#251e74]">
+              Office Of the Controller Of Examinations
+            </h1>
+            <h1 className="text-[#251e74]  font-bold text-xl">
+              مكتب مراقب الامتحانات
+            </h1>
+          </div>
+        </Link>
+
+        <div className="hidden lg:flex">
+          <div className="text-center">
+            <h1 className="font-bold text-[12px] text-center uppercase text-[#251e74]">
               Office Of the Controller Of Examinations
             </h1>
             <h1 className="text-[#251e74]  font-bold text-3xl">
-              مكتب المراقب الامتحانات
+              مكتب مراقب الامتحانات
             </h1>
           </div>
         </div>
+        <div className="justify-center hidden lg:flex items-center">
+          <a
+            href="https://www.facebook.com/DHIUniversity/"
+            className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-gray-900  rounded-lg "
+          >
+            <FontAwesomeIcon icon={faFacebook} className="h-4" />
+          </a>
+          <a
+            href="https://www.youtube.com/@dhmediayt"
+            className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-gray-900  rounded-lg "
+          >
+            <FontAwesomeIcon icon={faYoutube} className="h-4" />
+          </a>
+          <a
+            href="https://www.instagram.com/darulhudaislamicuniversity/"
+            className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-gray-900  rounded-lg "
+          >
+            <FontAwesomeIcon icon={faInstagram} className="h-4" />
+          </a>
+          <a
+            href="https://twitter.com/DarulHudaIU"
+            className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-gray-900  rounded-lg "
+          >
+            <FontAwesomeIcon icon={faTwitter} className="h-4" />
+          </a>
+        </div>
       </div>
-     
     </div>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,10 @@ import {
   faBell,
   faBook,
   faBookAtlas,
+  faGraduationCap,
   faPen,
   faPerson,
-  faSearch
+  faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Swiper from "./components/Swiper";
@@ -12,22 +13,23 @@ import Swiper from "./components/Swiper";
 export default function Home() {
   return (
     <>
-      <section className="m-3" id="home">
+      <section className="m-3 mt-4" id="home">
         <div className="mt-10">
           <div className="lg:grid lg:grid-flow-row gap-8 items-center text-neutral-600 sm:grid-cols-1  lg:grid-cols-3">
-            <div className="bg-gray-200 flex flex-col m-auto py-10 items-center justify-center text-center mx-2 rounded-[10px]">
-              <h2 className="font-bold  text-xl  uppercase">
+            <div className="bg-gray-900 bg-opacity-60 flex flex-col m-auto py-10 items-center justify-center text-center mx-2 rounded-[10px]">
+              <h2 className="font-bold  text-white text-xl  capitalize">
                 Office of Examinations
                 <span className="ml-2 ">(DHIU)</span>
               </h2>
-              <p className="lg:ml-[10px] text-sm p-3 text-center">
-                The DHIU Examination Board conducts and controls all the
-                examinations and its procedures, arranges for the valuation of
-                answer scripts, processes marks, and announces results and
-                issues secondary, senior secondary, degree and P.G certificates.
-                The valuation of answer scripts is carried out at centralized
-                valuation camps and results of all examinations are announced in
-                time.
+              <p className="lg:ml-[10px] mt-3 text-white text-sm px-6">
+                The DHIU Examination Board manages exam-related tasks, from
+                scheduling to conducting exams transparently. It evaluates
+                scripts, publishes results, and issues certificates for
+                Secondary, Senior Secondary, UG, and PG levels. Led by the Chief
+                Examination Controller. The Board, which consists of reputed
+                scholars and academicians, is operating under the Chief
+                Examination Controller who coordinates examination procedures at
+                all affiliated colleges and study centres
               </p>
             </div>
             <Swiper />
@@ -35,7 +37,7 @@ export default function Home() {
             <div className="flex flex-col items-center py-2  justify-center text-center mx-2 rounded-[10px]">
               <div
                 id="toast-default"
-                className="flex items-center hover:bg-gray-800 cursor-pointer transition w-full m-2 p-4 text-gray-100 bg-gray-600 rounded-lg shadow "
+                className="flex items-center hover:bg-gray-800 cursor-pointer transition w-full m-2 p-4 text-gray-100 bg-gray-900 rounded-lg shadow "
                 role="alert"
               >
                 <div className="inline-flex items-center  justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg ">
@@ -47,7 +49,7 @@ export default function Home() {
               </div>
               <div
                 id="toast-default"
-                className="flex items-center w-full m-2 p-4 hover:bg-gray-800 cursor-pointer transition text-gray-100 bg-gray-600 rounded-lg shadow "
+                className="flex items-center w-full m-2 p-4 hover:bg-gray-800 cursor-pointer transition text-gray-100 bg-gray-900 rounded-lg shadow "
                 role="alert"
               >
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg ">
@@ -60,7 +62,7 @@ export default function Home() {
               </div>
               <div
                 id="toast-default"
-                className="flex items-center w-full m-2 p-4 hover:bg-gray-800 cursor-pointer transition text-gray-100 bg-gray-600 rounded-lg shadow "
+                className="flex items-center w-full m-2 p-4 hover:bg-gray-800 cursor-pointer transition text-gray-100 bg-gray-900 rounded-lg shadow "
                 role="alert"
               >
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg ">
@@ -73,36 +75,42 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="lg:flex lg:mt-20 justify-center ">
-            <a href="#">
-              <div className="flex mt-2 bg-[#2E97A7] p-2 mx-3 px-7 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#2E97A7] border border-[#2E97A7] transition-colors duration-300">
-                <FontAwesomeIcon icon={faSearch} className="h-6 mr-2" />
-                <p className="uppercase my-3">enquiry portal</p>
+          <div className="lg:flex lg:mt-10 justify-center ">
+            <a href="https://dhexams.000webhostapp.com/">
+              <div className="flex mt-2 bg-[#11373d] py-5 mx-1 px-8 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#11373d] border border-[#11373d] transition-colors duration-300">
+                <FontAwesomeIcon icon={faSearch} className="h-4 mr-2" />
+                <p className="capitalize ">enquiry portal</p>
               </div>
             </a>
-            <a href="#">
-              <div className="flex mt-2 bg-[#2E97A7] p-2 mx-3 px-7 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#2E97A7] border border-[#2E97A7] transition-colors duration-300">
-                <FontAwesomeIcon icon={faBook} className="h-6 mr-2" />
-                <p className="uppercase my-3">result portal</p>
+            <a href="https://result.dhiu.in/">
+              <div className="flex mt-2 bg-[#11373d] py-5 mx-1 px-8 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#11373d] border border-[#11373d] transition-colors duration-300">
+                <FontAwesomeIcon icon={faBook} className="h-4 mr-2" />
+                <p className="capitalize ">result portal</p>
               </div>
             </a>
 
             <a href="#">
-              <div className="flex mt-2 bg-[#2E97A7] p-2 mx-3 px-7 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#2E97A7] border border-[#2E97A7] transition-colors duration-300">
-                <FontAwesomeIcon icon={faPen} className="h-6 mr-2" />
-                <p className="uppercase my-3">admission portal</p>
+              <div className="flex mt-2 bg-[#11373d] py-5 mx-1 px-8 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#11373d] border border-[#11373d] transition-colors duration-300">
+                <FontAwesomeIcon icon={faPen} className="h-4 mr-2" />
+                <p className="capitalize ">admission portal</p>
               </div>
             </a>
-            <a href="#">
-              <div className="flex mt-2 bg-[#2E97A7] p-2 mx-3 px-7 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#2E97A7] border border-[#2E97A7] transition-colors duration-300">
-                <FontAwesomeIcon icon={faPerson} className="h-6 mr-2" />
-                <p className="uppercase my-3">parent portal</p>
+            <a href="https://pg.dhiu.in/studentlogin">
+              <div className="flex mt-2 bg-[#11373d] py-5 mx-1 px-8 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#11373d] border border-[#11373d] transition-colors duration-300">
+                <FontAwesomeIcon icon={faGraduationCap} className="h-4 mr-2" />
+                <p className="capitalize ">Student's Portal PG</p>
               </div>
             </a>
-            <a href="#">
-              <div className="flex mt-2 bg-[#2E97A7] p-2 mx-3 px-7 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#2E97A7] border border-[#2E97A7] transition-colors duration-300">
-                <FontAwesomeIcon icon={faBookAtlas} className="h-6 mr-2" />
-                <p className="uppercase my-3">academic portal</p>
+            <a href="https://portal.dhiu.in/parent">
+              <div className="flex mt-2 bg-[#11373d] py-5 mx-1 px-8 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#11373d] border border-[#11373d] transition-colors duration-300">
+                <FontAwesomeIcon icon={faPerson} className="h-4 mr-2" />
+                <p className="capitalize ">parent portal</p>
+              </div>
+            </a>
+            <a href="https://portal.dhiu.in/">
+              <div className="flex mt-2 bg-[#11373d] py-5 mx-1 px-8 rounded-[30px] cursor-pointer  items-center justify-center text-white  hover:bg-white hover:text-[#11373d] border border-[#11373d] transition-colors duration-300">
+                <FontAwesomeIcon icon={faBookAtlas} className="h-4 mr-2" />
+                <p className="capitalize ">academic portal</p>
               </div>
             </a>
           </div>
