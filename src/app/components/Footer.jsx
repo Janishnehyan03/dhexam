8d5ef4f15dbe1ad4277bedad67f2d04ce7164468
Janishnehyan03 <nehyanjanish@gmail.com>
2023-11-15 19:06:31 +0530

    footer on the bottom fixed

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -8,7 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-2 absolute bottom-0 w-full">
+    <footer className="bg-gray-800 text-white py-2  w-full">
       <div className="w-full mx-auto justify-center flex items-center">
         <a target="_blank" href="https://www.facebook.com/DHIUniversity/" className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-gray-200  rounded-lg ">
           <FontAwesomeIcon icon={faFacebook} className="h-4" />
